010b18015e0ec23a096875c32a2fd73a7478c08c
Al-Evst <al.evst96@gmail.com>
2024-12-10 19:43:20 +0100

    Update pass.py

--- pass.py
+++ pass.py
@@ -1,61 +1,42 @@
-print("Введи пароль: ")
-password = str(input())
+import urwid
 
-def is_very_long(password):
-    if len(password) >= 12:
+def is_very_long(passwd:str):
+    if len(passwd) > 12:
         return True
     else:
         return False
 
-def has_digit(password):
-  for frm in password:
-    if frm.isdigit(password):
-      return True
-  return False
+def has_digit(passwd:str):
+    return any(symbol.isdigit() for symbol in passwd)
 
-# def raiting(password):
-#   score = 0
-#   if is_very_long(password):
-#     score += 2
-#   elif has_digit(password):
-#     score += 2
-#   elif has_letters(password):
-#     score += 2
-#   elif has_upper_letters(password):
-#     score += 2
-#   elif has_lower_letters(password):
-#     score += 2
-#   return score
-  
-def has_letters(password):
-  for lt in password:
-    if lt.isalpha():
-      return True
-  return False
+def has_letters(passwd:str):
+    return any(symbol.isalpha() for symbol in passwd)
 
-def has_upper_letters(password):
-  for up in password:
-    if up.isupper():
-      return True
-  return False
+def has_upper_letters(passwd:str):
+    return any(symbol.isupper() for symbol in passwd)
 
-def has_lower_letters(password):
-  for lw in password:
-    if lw.islower():
-      return True
-  return False
+def has_lower_letters(passwd:str):
+    return any(symbol.islower() for symbol in passwd)
+    
+def has_symbols(passwd:str):
+    return any(not symbol.islower() and not symbol.isalpha() for symbol in passwd)
 
-# is_very_long()
-# has_digit()
-# has_letters()
-# has_upper_letters()
-# has_lower_letters()
-
-def call_fun():
-    some_func = [is_very_long, has_digit, has_letters, has_upper_letters,has_lower_letters]
+def rating(passwd, list_of_functions):
     score = 0
-    for func in some_func:
-        if func(password):
-            score += 2
+    for func in list_of_functions:
+        score += 2 if func(passwd) else 0
+    return score
+    
+def on_password_change(edit, new_password):
+    reply.set_text(f"Рейтинг пароля: {rating(new_password, functions)}")
 
-print(call_fun())
+
+functions = [is_very_long, has_digit, has_letters, has_upper_letters,
+             has_lower_letters, has_symbols]
+
+password = urwid.Edit('Введите пароль: ', mask='*')
+reply = urwid.Text("")
+menu = urwid.Pile([password, reply])
+menu = urwid.Filler(menu, valign='top')
+urwid.connect_signal(password, 'change', on_password_change)
+urwid.MainLoop(menu).run()
