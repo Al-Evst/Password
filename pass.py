4b50117621e8867a27e3fda5c4a6e31a54b6bb53
Al-Evst <al.evst96@gmail.com>
2024-12-12 13:37:53 +0100

    Update pass.py

--- pass.py
+++ pass.py
@@ -4,29 +4,37 @@ import urwid
 def is_very_long(passwd:str):
     return len(passwd) >= 12 
 
+
 def has_digit(passwd:str):
     return any(symbol.isdigit() for symbol in passwd)
+
 
 def has_letters(passwd:str):
     return any(symbol.isalpha() for symbol in passwd)
 
+
 def has_upper_letters(passwd:str):
     return any(symbol.isupper() for symbol in passwd)
 
+
 def has_lower_letters(passwd:str):
     return any(symbol.islower() for symbol in passwd)
-    
+
+
 def has_symbols(passwd:str):
     return any(not symbol.islower() and not symbol.isalpha() for symbol in passwd)
+
 
 def rating(passwd, list_of_functions):
     score = 0
     for func in list_of_functions:
         score += 2 if func(passwd) else 0
     return score
-    
+
+
 def on_password_change(edit, new_password):
     reply.set_text(f"Рейтинг пароля: {rating(new_password, functions)}")
+
 
 if __name__ == '__main__':
 
